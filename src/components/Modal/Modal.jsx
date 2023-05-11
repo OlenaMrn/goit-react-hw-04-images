0208a9e3b7bc9a71755969fd334b7850013a5a60
Olena <anyanyme@gmail.com>
2023-05-11 19:43:43 +0200

    update modal.js with hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,43 +1,42 @@
-import { Component } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 
 import styles from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
- 
+export function Modal({ largeImageURL, tags, onClose }) {
+  const handleClickBackdrop = useCallback(
+    event => {
+      if (event.target === event.currentTarget) {
+        onClose();
+      }
+    },
+    [onClose]
+  );
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+  useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+    window.addEventListener('keydown', handleKeyDown);
+     window.addEventListener('click', handleClickBackdrop);
 
-  handleKeyDown = event => {
-    if (event.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
+    return () => {
+       window.removeEventListener('keydown', handleKeyDown);
+       window.removeEventListener('click', handleClickBackdrop);
+    };
+  }, [onClose, handleClickBackdrop]);
 
-  handleClickBackdrop = event => {
-    if (event.target === event.currentTarget) {
-      this.props.onClose();
-    }
-  };
-
-  render() {
-    return createPortal(
-      <div className={styles.Overlay} onClick={this.handleClickBackdrop}>
-        <div className={styles.Modal}>
-          <img src={this.props.largeImageURL} alt="Large" />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <div className={styles.Overlay} onClick={handleClickBackdrop}>
+      <div className={styles.Modal}>
+        <img src={largeImageURL} alt="Large" />
+      </div>
+    </div>,
+    modalRoot
+  );
 }
-
-
